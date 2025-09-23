3fb9ed04e486decb39318670cda64ca0ba6995bc
Nolan Phan <149433038+exnonuser@users.noreply.github.com>
2025-09-23 16:42:47 -0700

    Added an HTTP endpoint

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.get("/hello", (req, res) => {
   res.json({ message: "API is working from 0100 Loop" });
 });
 
+app.get('/greetings', (req, res) => {
+  res.send('Hello from ParkIt!')
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server running at http://localhost:" + port);
