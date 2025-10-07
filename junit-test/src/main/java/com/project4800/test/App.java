5d3aba2761308ee98094f5b2437853d7d4d3e9b1
Solares10 <solaresmichelle22@gmail.com>
2025-10-07 15:49:51 -0700

    test: add Maven module (JUnit 4.12) with passing AppTest

---
+++ junit-test/src/main/java/com/project4800/test/App.java
@@ -0,0 +1,14 @@
+package com.project4800.test;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ junit-test/src/test/java/com/project4800/test/AppTest.java
@@ -0,0 +1,11 @@
+package com.project4800.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testTruth() {
+        assertTrue(true);
+    }
+}
