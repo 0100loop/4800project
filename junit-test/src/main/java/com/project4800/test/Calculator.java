79c3ce447240882875970bc9b6396e36b908a1e7
Nolan Phan <149433038+exnonuser@users.noreply.github.com>
2025-10-07 23:07:45 -0700

    Junit test Assignment 6
    Example Calculator and CalculatorTest classes

---
+++ junit-test/src/main/java/com/project4800/test/Calculator.java
@@ -0,0 +1,12 @@
+package com.project4800.test;
+
+public class Calculator {
+	public int subtract(int a, int b){
+		return a-b;
+	}
+	
+	public static void main(String args[]){
+		Calculator c = new Calculator();
+	}
+	
+}
