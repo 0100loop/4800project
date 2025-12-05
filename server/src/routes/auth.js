dfc926866fd6c8bde40312561a010204bce888cc
Nolan Phan <149433038+exnonuser@users.noreply.github.com>
2025-12-05 14:17:36 -0800

    N

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -164,7 +164,7 @@ router.get(
 
     // Redirect to frontend with token and name
     res.redirect(
-      `http://${FRONTEND_URL}/auth-success?token=${token}&name=${encodeURIComponent(name)}`
+      `https://${FRONTEND_URL}/auth-success?token=${token}&name=${encodeURIComponent(name)}`
     );
   }
 );
